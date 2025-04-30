bb759b71ea5b3791e06f6df9d1129c07eb954386
koffikoffi99 <koffikoffias23@gmail.com>
2025-04-30 09:41:47 +0000

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 rounded">
+        Hello Tailwind CSS !
+      </div>
+    </>
+  );
+}
+
+export default App;
